ad4973ff454adb285d16265b7df53129057b79c7
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-05-17 16:08:23 +0800

    added css for home and tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,20 +1,30 @@
-import Link from "next/link";
+ import Link from "next/link";
 
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
 const TicketsPage = () => {
   return (
-    <div>
-      {initialTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <h2 className="text-lg">{ticket.title}</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets in one place
+        </p>
+      </div>
 
-          <Link href={ticketPath(ticket.id)} className="text-sm underline">
-            View
-          </Link>
-        </div>
-      ))}
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+        {initialTickets.map((ticket) => (
+          <div key={ticket.id} className="w-full max-w-[420px]">
+            <h2 className="text-lg">{ticket.title}</h2>
+            <p className="text-sm truncate">{ticket.content}</p>
+
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
