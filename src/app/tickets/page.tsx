ba8e2b284d00fd4ea948669c5d9e501cbdf9e4ce
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-05-19 21:59:30 +0800

    extracted out ticket item component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,7 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
-
 import { Heading } from "@/components/heading";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
-};
+import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { TicketStatus } from "@/features/types";
 
 const TicketsPage = () => {
   return (
@@ -19,24 +10,13 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span className="flex items-center">{TICKET_ICONS[ticket.status as keyof typeof TICKET_ICONS]}</span>
-                <span className="truncate text-2xl font-semibold">{ticket.title}</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <span className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </span>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem
+            key={ticket.id}
+            ticket={{
+              ...ticket,
+              status: ticket.status as TicketStatus,
+            }}
+          />
         ))}
       </div>
     </div>
