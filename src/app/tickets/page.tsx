f42669ee1b372d1f22913c216154ce9b38d69f47
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-05-24 10:56:14 +0800

    added error.tsx to tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -8,7 +8,6 @@ const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="All your tickets in one place" />
-
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
