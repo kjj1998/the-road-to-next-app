191c4f1c00c1f3358c20a1c13f5b3385a2ecd67f
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-05-29 07:31:14 +0800

    added form validation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,27 +2,38 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formDate: FormData
 ) => {
-  const data = {
-    title: formDate.get("title") as string,
-    content: formDate.get("content") as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formDate.get("title"),
+      content: formDate.get("content"),
+    });
 
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { message: "Something went wrong" };
+  }
 
   revalidatePath(ticketsPath());
 
