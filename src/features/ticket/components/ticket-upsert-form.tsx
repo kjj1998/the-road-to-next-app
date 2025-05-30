fc04e1aa1492dce21cb1e2e94febd38ddef13d3d
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-05-30 22:00:30 +0800

    added action callbacks using custom hooks

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -3,6 +3,7 @@
 import { useActionState } from "react";
 
 import { FieldError } from "@/components/form/field-error";
+import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
@@ -21,6 +22,15 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
     upsertTicket.bind(null, ticket?.id),
     EMPTY_ACTION_STATE
   );
+
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => { 
+      console.log(actionState.message);
+    },
+    onError: ({ actionState }) => { 
+      console.log(actionState.message);
+    },
+  });
 
   return (
     <form action={action} className="flex flex-col gap-y-2">
